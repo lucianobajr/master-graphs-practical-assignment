0f17b42a08884c2f8b0a17bbfa657f21d9ddc157
lucianobajr <luciano.alcantara@ufv.br>
2024-05-27 09:03:17 -0300

    add(+): start solver

--- 1205/main.py
+++ 1205/main.py
@@ -1,4 +1,18 @@
 import sys
+from collections import defaultdict
+
+def soldiers_probability_success(
+        N:int, 
+        M:int, 
+        K:int, 
+        P:float, 
+        roads:defaultdict[any, list], 
+        shooter_positions:list[int], 
+        starting_point:int, 
+        destination_point:int
+    ) -> float:
+
+    return 1.000
 
 def run():
     input = sys.stdin.read
@@ -6,25 +20,38 @@ def run():
 
     index = 0
     
-    while True:
-        N,M,K,P = map(float, data[index].split())
+    while index < len(data):
+        if data[index].strip() == '':
+            break
+        
+        # Lê N, M, K, P
+        N, M, K, P = data[index].split()
+        N, M, K = int(N), int(M), int(K)
+        P = float(P)
+
         index += 1
 
-        for _ in range(int(M)):
-            i, j = data[index].split() # idiomas de origem e destino
+        # Construção do grafo
+        roads = defaultdict(list)
+        for _ in range(M):
+            i, j = map(int, data[index].split())
+            roads[i].append(j)
+            roads[j].append(i)
             index += 1
 
-        # numero de atiradores
+        # Lê o número de atiradores e suas posições
         A = int(data[index].split()[0])
-
-        # posições dos atiradores
         shooter_positions = list(map(int, data[index].split()[1:]))
-        
         index += 1
 
-        starting_point, destination_point = map(int,data[index].split())
+        # Lê o ponto de partida e destino
+        starting_point, destination_point = map(int, data[index].split())
         index += 1
-        print(starting_point,destination_point)
+
+        probability_success_value = soldiers_probability_success
+
+        # resultado com três casas decimais
+        print("{:.3f}".format(probability_success_value))
 
 if __name__ == "__main__":
     run()
